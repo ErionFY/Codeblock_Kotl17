6472c33337f394c0c3dfe8e7cc95c8689684e82f
ErionFY <goncharov.timofej@gmail.com>
2022-04-28 18:32:23 +0700

    я создал 2-3 кнопочки
    buttons+activity

--- app/src/main/java/com/example/mobiledev17/MainActivity.kt
+++ app/src/main/java/com/example/mobiledev17/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.mobiledev17
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val startButton: Button = findViewById(R.id.startButton)
+        startButton.setOnClickListener {
+            val intent = Intent(this, codeblockActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val quitButton: Button = findViewById(R.id.quitButton)
+        quitButton.setOnClickListener {
+            this.finish()
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/example/mobiledev17/codeblockActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mobiledev17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class codeblockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_codeblock)
+
+
+
+    }
+}
